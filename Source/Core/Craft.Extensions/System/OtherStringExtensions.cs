97cf6f69bad5d5a8f7b7cbe5aa9e8846b76226c5
Sandeep.Sharma <Sandeepksharma15@gmail.com>
2026-09-22 13:21:49 +0530

    refactor: reorganize and extend string extension methods
    Added CountOccurrences, IsAlphabetic, IsAlphanumeric, IsBase64String, and IsNumeric methods. Refactored and moved ToCamelCase, ToKebabCase, Truncate, and TruncateWithEllipsis for clarity. Reverse now simply reverses the string; ToMd5 computes the MD5 hash. Grouped public methods with #region, improved organization, and adopted modern C# features with better null/empty checks.

--- Source/Core/Craft.Extensions/System/OtherStringExtensions.cs
+++ Source/Core/Craft.Extensions/System/OtherStringExtensions.cs
@@ -6,6 +6,79 @@ namespace Craft.Extensions.System;
 
 public static class OtherStringExtensions
 {
+    #region Public Methods
+
+    /// <summary>
+    /// Counts the number of occurrences of a substring within the string.
+    /// </summary>
+    public static int CountOccurrences(this string? str, string substring, StringComparison comparisonType = StringComparison.Ordinal)
+    {
+        if (string.IsNullOrEmpty(str) || string.IsNullOrEmpty(substring))
+            return 0;
+
+        int count = 0;
+        int index = 0;
+
+        while ((index = str.IndexOf(substring, index, comparisonType)) != -1)
+        {
+            count++;
+            index += substring.Length;
+        }
+
+        return count;
+    }
+
+    /// <summary>
+    /// Determines whether the string contains only letters.
+    /// </summary>
+    public static bool IsAlphabetic(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return false;
+
+        return str.All(char.IsLetter);
+    }
+
+    /// <summary>
+    /// Determines whether the string contains only letters and digits.
+    /// </summary>
+    public static bool IsAlphanumeric(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return false;
+
+        return str.All(char.IsLetterOrDigit);
+    }
+
+    /// <summary>
+    /// Determines whether the specified string is a valid Base64-encoded string.
+    /// </summary>
+    /// <remarks>A valid Base64 string must contain only characters allowed in Base64 encoding  and must have
+    /// a length that is a multiple of 4. This method returns <see langword="false"/>  for null, empty, or whitespace
+    /// strings.</remarks>
+    /// <param name="s">The string to validate as Base64-encoded.</param>
+    /// <returns><see langword="true"/> if the specified string is a valid Base64-encoded string;  otherwise, <see
+    /// langword="false"/>.</returns>
+    public static bool IsBase64String(this string s)
+    {
+        if (s.IsNullOrWhiteSpace()) return false;
+
+        Span<byte> buffer = new byte[s.Length + 1];
+
+        return Convert.TryFromBase64String(s, buffer, out _);
+    }
+
+    /// <summary>
+    /// Determines whether the string contains only digits.
+    /// </summary>
+    public static bool IsNumeric(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return false;
+
+        return str.All(char.IsDigit);
+    }
+
     /// <summary>
     /// Normalizes the line endings in the current string to the platform-specific line ending.
     /// </summary>
@@ -111,6 +184,43 @@ public static class OtherStringExtensions
     }
 
     /// <summary>
+    /// Reverses the characters in the string.
+    /// </summary>
+    public static string? Reverse(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        var charArray = str.ToCharArray();
+        Array.Reverse(charArray);
+        return new string(charArray);
+    }
+
+    /// <summary>
+    /// Converts a string to camelCase (e.g., "hello world" -> "helloWorld").
+    /// </summary>
+    public static string? ToCamelCase(this string? str)
+    {
+        var pascal = str.ToPascalCase();
+
+        return string.IsNullOrEmpty(pascal)
+            ? pascal
+            : char.ToLowerInvariant(pascal[0]) + pascal[1..];
+    }
+
+    /// <summary>
+    /// Converts a string to kebab-case (e.g., "HelloWorld" -> "hello-world").
+    /// </summary>
+    public static string? ToKebabCase(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        var result = Regex.Replace(str, "([a-z0-9])([A-Z])", "$1-$2");
+        return result.Replace(' ', '-').Replace('_', '-').ToLowerInvariant();
+    }
+
+    /// <summary>
     /// Computes the MD5 hash of the current string and returns it as a hexadecimal string.
     /// </summary>
     public static string ToMd5(this string str)
@@ -129,21 +239,33 @@ public static class OtherStringExtensions
     }
 
     /// <summary>
-    /// Determines whether the specified string is a valid Base64-encoded string.
+    /// Converts a string to PascalCase (e.g., "hello world" -> "HelloWorld").
     /// </summary>
-    /// <remarks>A valid Base64 string must contain only characters allowed in Base64 encoding  and must have
-    /// a length that is a multiple of 4. This method returns <see langword="false"/>  for null, empty, or whitespace
-    /// strings.</remarks>
-    /// <param name="s">The string to validate as Base64-encoded.</param>
-    /// <returns><see langword="true"/> if the specified string is a valid Base64-encoded string;  otherwise, <see
-    /// langword="false"/>.</returns>
-    public static bool IsBase64String(this string s)
+    public static string? ToPascalCase(this string? str)
     {
-        if (s.IsNullOrWhiteSpace()) return false;
+        if (string.IsNullOrEmpty(str))
+            return str;
 
-        Span<byte> buffer = new byte[s.Length + 1];
+        var words = str.Split([' ', '_', '-'], StringSplitOptions.RemoveEmptyEntries);
+        var result = new StringBuilder();
 
-        return Convert.TryFromBase64String(s, buffer, out _);
+        foreach (var word in words)
+            if (word.Length > 0)
+                result.Append(char.ToUpperInvariant(word[0]) + word[1..].ToLowerInvariant());
+
+        return result.ToString();
+    }
+
+    /// <summary>
+    /// Converts a string to snake_case (e.g., "HelloWorld" -> "hello_world").
+    /// </summary>
+    public static string? ToSnakeCase(this string? str)
+    {
+        if (string.IsNullOrEmpty(str))
+            return str;
+
+        var result = Regex.Replace(str, "([a-z0-9])([A-Z])", "$1_$2");
+        return result.Replace(' ', '_').Replace('-', '_').ToLowerInvariant();
     }
 
     /// <summary>
@@ -170,123 +292,5 @@ public static class OtherStringExtensions
             : str[..(maxLength - ellipsis.Length)] + ellipsis;
     }
 
-    /// <summary>
-    /// Converts a string to PascalCase (e.g., "hello world" -> "HelloWorld").
-    /// </summary>
-    public static string? ToPascalCase(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return str;
-
-        var words = str.Split([' ', '_', '-'], StringSplitOptions.RemoveEmptyEntries);
-        var result = new StringBuilder();
-
-        foreach (var word in words)
-            if (word.Length > 0)
-                result.Append(char.ToUpperInvariant(word[0]) + word[1..].ToLowerInvariant());
-
-        return result.ToString();
-    }
-
-    /// <summary>
-    /// Converts a string to camelCase (e.g., "hello world" -> "helloWorld").
-    /// </summary>
-    public static string? ToCamelCase(this string? str)
-    {
-        var pascal = str.ToPascalCase();
-
-        return string.IsNullOrEmpty(pascal)
-            ? pascal
-            : char.ToLowerInvariant(pascal[0]) + pascal[1..];
-    }
-
-    /// <summary>
-    /// Converts a string to snake_case (e.g., "HelloWorld" -> "hello_world").
-    /// </summary>
-    public static string? ToSnakeCase(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return str;
-
-        var result = Regex.Replace(str, "([a-z0-9])([A-Z])", "$1_$2");
-        return result.Replace(' ', '_').Replace('-', '_').ToLowerInvariant();
-    }
-
-    /// <summary>
-    /// Converts a string to kebab-case (e.g., "HelloWorld" -> "hello-world").
-    /// </summary>
-    public static string? ToKebabCase(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return str;
-
-        var result = Regex.Replace(str, "([a-z0-9])([A-Z])", "$1-$2");
-        return result.Replace(' ', '-').Replace('_', '-').ToLowerInvariant();
-    }
-
-    /// <summary>
-    /// Reverses the characters in the string.
-    /// </summary>
-    public static string? Reverse(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return str;
-
-        var charArray = str.ToCharArray();
-        Array.Reverse(charArray);
-        return new string(charArray);
-    }
-
-    /// <summary>
-    /// Counts the number of occurrences of a substring within the string.
-    /// </summary>
-    public static int CountOccurrences(this string? str, string substring, StringComparison comparisonType = StringComparison.Ordinal)
-    {
-        if (string.IsNullOrEmpty(str) || string.IsNullOrEmpty(substring))
-            return 0;
-
-        int count = 0;
-        int index = 0;
-
-        while ((index = str.IndexOf(substring, index, comparisonType)) != -1)
-        {
-            count++;
-            index += substring.Length;
-        }
-
-        return count;
-    }
-
-    /// <summary>
-    /// Determines whether the string contains only digits.
-    /// </summary>
-    public static bool IsNumeric(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return false;
-
-        return str.All(char.IsDigit);
-    }
-
-    /// <summary>
-    /// Determines whether the string contains only letters.
-    /// </summary>
-    public static bool IsAlphabetic(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return false;
-
-        return str.All(char.IsLetter);
-    }
-
-    /// <summary>
-    /// Determines whether the string contains only letters and digits.
-    /// </summary>
-    public static bool IsAlphanumeric(this string? str)
-    {
-        if (string.IsNullOrEmpty(str))
-            return false;
-
-        return str.All(char.IsLetterOrDigit);
-    }
+    #endregion Public Methods
 }

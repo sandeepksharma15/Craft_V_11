1fca1551d834286b3b8caa55541d743923952493
Monk <MonkxSandeep@gmail.com>
2026-09-22 12:09:13 +0530

    Refactor EnumExtensions for .NET 11

--- Source/Core/Craft.Extensions/System/EnumExtensions.cs
+++ Source/Core/Craft.Extensions/System/EnumExtensions.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using System.Globalization;
+using System.ComponentModel;
 
 #pragma warning disable IDE0130 // Namespace does not match folder structure
 namespace System;
@@ -7,203 +6,151 @@ namespace System;
 
 public static class EnumExtensions
 {
-    /// <summary>
-    /// Retrieves a list of all values in the specified enumeration type, ordered by their underlying integer values.
-    /// </summary>
-    public static List<T> GetOrderedEnumValues<T>() where T : struct, Enum
-        => [.. Enum
-            .GetValues<T>()
-            .OrderBy(x => Convert.ToInt32(x, CultureInfo.InvariantCulture))];
-
-    /// <summary>
-    /// Retrieves the extreme value of an enumeration, either the highest or the lowest, based on the specified condition.
-    /// </summary>
-    public static T GetExtremeEnumValue<T>(bool highest = true) where T : struct, Enum
-        => Enum.GetValues<T>()
-            .OrderBy(x => Convert.ToInt32(x, CultureInfo.InvariantCulture) * (highest ? -1 : 1))
-            .First();
-
-    /// <summary>
-    /// Retrieves the highest value defined in the specified enumeration type.
-    /// </summary>
-    public static T GetHighestEnumValue<T>() where T : struct, Enum
-        => GetExtremeEnumValue<T>(highest: true);
-
-    /// <summary>
-    /// Retrieves the lowest value of the specified enumeration type.
-    /// </summary>
-    public static T GetLowestEnumValue<T>() where T : struct, Enum
-        => GetExtremeEnumValue<T>(highest: false);
-
-    /// <summary>
-    /// Gets a dictionary of all enum values and their descriptions.
-    /// </summary>
-    public static Dictionary<T, string> GetDescriptions<T>() where T : struct, Enum
-        => Enum.GetValues<T>().ToDictionary(value => value, value => value.GetDescription());
-
-    /// <summary>
-    /// Gets a dictionary of all enum values and their names.
-    /// </summary>
-    public static Dictionary<T, string> GetNames<T>() where T : struct, Enum
-        => Enum.GetValues<T>().ToDictionary(value => value, value => value.ToString()!);
-
-    /// <summary>
-    /// Gets an array of all enum values.
-    /// </summary>
-    public static T[] GetValues<T>() where T : struct, Enum
-        => Enum.GetValues<T>();
-
-    /// <summary>
-    /// Retrieves the next value in the enumeration sequence for the specified enum value.
-    /// </summary>
-    public static T GetNextEnumValue<T>(this T enumValue) where T : struct, Enum
+    extension<T>(T value) where T : struct, Enum
     {
-        var values = GetOrderedEnumValues<T>();
-
-        var currentIndex = values.IndexOf(enumValue);
-
-        return currentIndex >= 0 && currentIndex < values.Count - 1
-            ? values[currentIndex + 1]
-            : values[0];
-    }
-
-    /// <summary>
-    /// Retrieves the previous value in the enumeration relative to the specified value.
-    /// </summary>
-    public static T GetPrevEnumValue<T>(this T enumValue) where T : struct, Enum
-    {
-        var values = GetOrderedEnumValues<T>();
-
-        var currentIndex = values.IndexOf(enumValue);
-
-        return currentIndex > 0 && currentIndex < values.Count
-            ? values[currentIndex - 1]
-            : values[^1];
-    }
-
-    /// <summary>
-    /// Retrieves the description attribute of the specified enumeration value.
-    /// </summary>
-    public static string GetDescription<T>(this T enumValue) where T : struct, Enum
-    {
-        var memberInfo = typeof(T).GetMember(enumValue.ToString()!);
-
-        if (memberInfo is { Length: > 0 })
+        public string GetDescription()
         {
-            var attributes = memberInfo[0]
-                .GetCustomAttributes(typeof(DescriptionAttribute), false);
+            var name = value.ToString();
+            var member = typeof(T).GetMember(name).FirstOrDefault();
 
-            return attributes is { Length: > 0 }
-                ? ((DescriptionAttribute)attributes[0]).Description
-                : enumValue.ToString()!;
+            return member?.GetCustomAttributes<DescriptionAttribute>().FirstOrDefault()?.Description
+                ?? name;
         }
 
-        return string.Empty;
+        public string GetName() => value.ToString();
+
+        public bool TryGetSingleDescription(out string? description)
+        {
+            var name = value.ToString();
+
+            if (name.Contains(',', StringComparison.Ordinal))
+            {
+                description = null;
+                return false;
+            }
+
+            description = value.GetDescription();
+            return true;
+        }
+
+        public bool TryGetSingleName(out string? name)
+        {
+            var valueName = value.ToString();
+
+            if (valueName.Contains(',', StringComparison.Ordinal))
+            {
+                name = null;
+                return false;
+            }
+
+            name = valueName;
+            return true;
+        }
+
+        public T GetNextEnumValue()
+        {
+            var values = Enum.GetValues<T>();
+            var index = Array.IndexOf(values, value);
+
+            return index >= 0 && index < values.Length - 1
+                ? values[index + 1]
+                : values[0];
+        }
+
+        public T GetPrevEnumValue()
+        {
+            var values = Enum.GetValues<T>();
+            var index = Array.IndexOf(values, value);
+
+            return index > 0
+                ? values[index - 1]
+                : values[^1];
+        }
+
+        public IEnumerable<T> GetFlags()
+        {
+            if (!typeof(T).IsDefined(typeof(FlagsAttribute), false))
+                return [];
+
+            var numericValue = Convert.ToUInt64(value);
+
+            return Enum.GetValues<T>()
+                .Where(flag => flag is not null &&
+                               !EqualityComparer<T>.Default.Equals(flag, default) &&
+                               (numericValue & Convert.ToUInt64(flag)) == Convert.ToUInt64(flag));
+        }
+
+        public bool IsSet(T flags)
+            => (Convert.ToUInt64(value) & Convert.ToUInt64(flags)) == Convert.ToUInt64(flags);
+
+        public string ToStringInvariant() => value.ToString();
     }
 
-    /// <summary>
-    /// Retrieves all individual flags that are set in the specified enumeration value.
-    /// </summary>
-    public static IEnumerable<T> GetFlags<T>(this T enumValue) where T : struct, Enum
-        => GetValues<T>().Where(item => enumValue.HasFlag(item));
-
-    /// <summary>
-    /// Determines whether the specified flag or combination of flags is set in the current enumeration value.
-    /// </summary>
-    public static bool IsSet<T>(this T enumValue, Enum matchTo) where T : struct, Enum
-        => (Convert.ToUInt32(enumValue, CultureInfo.InvariantCulture) & Convert.ToUInt32(matchTo, CultureInfo.InvariantCulture)) != 0;
-
-    /// <summary>
-    /// Gets the name of a specific enum value.
-    /// </summary>
-    public static string GetName<T>(this T enumValue) where T : struct, Enum
+    extension<T>(string value) where T : struct, Enum
     {
-        var names = GetNames<T>();
+        public T ToEnum(bool ignoreCase = true)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(value);
+            return Enum.Parse<T>(value, ignoreCase);
+        }
 
-        return names.TryGetValue(enumValue, out var result)
-            ? result
-            : string.Join(',', enumValue.GetFlags().Select(x => names[x]));
+        public bool Contains(T flags)
+        {
+            if (string.IsNullOrEmpty(value))
+                return false;
+
+            var names = flags.GetFlags()
+                .Select(flag => flag.ToString())
+                .Where(name => !string.IsNullOrEmpty(name));
+
+            return flags.ToString().Contains(',', StringComparison.Ordinal)
+                ? names.Any(name => value.Contains(name, StringComparison.InvariantCultureIgnoreCase))
+                : value.Contains(flags.ToString(), StringComparison.InvariantCultureIgnoreCase);
+        }
     }
 
-    /// <summary>
-    /// Tries to get the description of a specific enum value.
-    /// </summary>
-    public static bool TryGetSingleDescription<T>(this T enumValue, out string? result) where T : struct, Enum
-        => GetDescriptions<T>().TryGetValue(enumValue, out result);
-
-    /// <summary>
-    /// Tries to get the name of a specific enum value.
-    /// </summary>
-    public static bool TryGetSingleName<T>(this T enumValue, out string? result) where T : struct, Enum
-        => GetNames<T>().TryGetValue(enumValue, out result);
-
-    /// <summary>
-    /// Converts the specified enumeration value to its string representation using the invariant culture.
-    /// </summary>
-    public static string ToStringInvariant<T>(this T enumValue) where T : struct, Enum
-        => enumValue.GetName();
-
-    /// <summary>
-    /// Validates that the specified generic type parameter <typeparamref name="T"/> is an enumeration type.
-    /// </summary>
-    public static void ValidateEnumType<T>() where T : struct
+    extension<T>(T _) where T : struct, Enum
     {
-        if (!typeof(T).IsEnum)
-            throw new Exception("T must be an Enumeration type.");
+        public static List<T> GetOrderedEnumValues()
+            => [.. Enum.GetValues<T>()];
+
+        public static T GetHighestEnumValue()
+            => Enum.GetValues<T>().Max();
+
+        public static T GetLowestEnumValue()
+            => Enum.GetValues<T>().Min();
+
+        public static Dictionary<T, string> GetDescriptions()
+            => Enum.GetValues<T>()
+                .GroupBy(value => value)
+                .ToDictionary(group => group.Key, group => group.Key.GetDescription());
+
+        public static Dictionary<T, string> GetNames()
+            => Enum.GetValues<T>()
+                .GroupBy(value => value)
+                .ToDictionary(group => group.Key, group => group.Key.ToString());
+
+        public static T[] GetValues() => Enum.GetValues<T>();
     }
 
-    /// <summary>
-    /// Converts the specified integer value to the corresponding enumeration value of type <typeparamref name="T"/>.
-    /// </summary>
-    public static T ToEnum<T>(this int value) where T : struct
+    extension(Type enumType)
     {
-        ValidateEnumType<T>();
+        public List<(string Name, object Value)> GetEnumNameValuePairs()
+        {
+            var underlyingType = Nullable.GetUnderlyingType(enumType) ?? enumType;
 
-        return (T)Enum.ToObject(typeof(T), value);
+            if (!underlyingType.IsEnum)
+                return [];
+
+            return [.. Enum.GetValues(underlyingType)
+                .Cast<object>()
+                .Select(value => (Name: value.ToString() ?? string.Empty, Value: value))];
+        }
     }
 
-    /// <summary>
-    /// Converts the specified string representation of the name or numeric value of an enumeration to the equivalent enumeration value.
-    /// </summary>
-    public static T ToEnum<T>(this string value, bool ignoreCase = true) where T : struct
+    extension(int value)
     {
-        if (string.IsNullOrEmpty(value))
-            throw new ArgumentNullException(nameof(value));
-
-        ValidateEnumType<T>();
-
-        return Enum.Parse<T>(value, ignoreCase);
-    }
-
-    /// <summary>
-    /// Determines whether the specified flag or any of the flags in the specified enumeration value are contained within the current value.
-    /// </summary>
-    public static bool Contains<T>(this string value, T flags) where T : struct, Enum
-    {
-        return flags.TryGetSingleName(out var flagValue) && flagValue != null
-            ? value.Contains(flagValue, StringComparison.InvariantCultureIgnoreCase)
-            : flags
-            .GetFlags()
-            .Any(item => value.Contains(item.ToStringInvariant(), StringComparison.InvariantCultureIgnoreCase));
-    }
-
-    /// <summary>
-    /// Gets all enum values for a given enum type along with their names.
-    /// </summary>
-    /// <param name="enumType">The enum type to get values from.</param>
-    /// <returns>A list of tuples containing the name and value of each enum member. Returns an empty list if the type is not an enum.</returns>
-    /// <remarks>
-    /// This method handles nullable enum types by extracting the underlying enum type.
-    /// </remarks>
-    public static List<(string Name, object Value)> GetEnumNameValuePairs(this Type enumType)
-    {
-        var underlyingType = Nullable.GetUnderlyingType(enumType) ?? enumType;
-
-        if (!underlyingType.IsEnum)
-            return [];
-
-        return [.. Enum.GetValues(underlyingType)
-            .Cast<object>()
-            .Select(value => (Name: value.ToString() ?? string.Empty, Value: value))];
+        public T ToEnum<T>() where T : struct, Enum
+            => (T)Enum.ToObject(typeof(T), value);
     }
 }

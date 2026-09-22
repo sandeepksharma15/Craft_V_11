8dbb25bafca112b9770ed7106fbdc0db2d149c53
Monk <MonkxSandeep@gmail.com>
2026-09-22 13:04:08 +0530

    Add decimal byte-size conversions

--- Source/Core/Craft.Extensions/System/NumericExtensions.cs
+++ Source/Core/Craft.Extensions/System/NumericExtensions.cs
@@ -90,11 +90,20 @@ public static class NumericExtensions
 
     extension(long bytes)
     {
+        public double ToKilobytes()
+            => bytes / 1_000d;
+
         public double ToKibibytes()
             => bytes / 1024d;
 
+        public double ToMegabytes()
+            => bytes / 1_000_000d;
+
         public double ToMebibytes()
             => bytes / (1024d * 1024d);
+
+        public double ToGigabytes()
+            => bytes / 1_000_000_000d;
 
         public double ToGibibytes()
             => bytes / (1024d * 1024d * 1024d);
